1c3ebefbe1416bf75c4364bfc3bfb69674de0b67
Camilla <logikasciuro@gmail.com>
2023-09-09 17:18:39 +0000

    first commit

---
+++ src/main/java/com/ecureuill/rpgbattle/RpgbattleApplication.java
@@ -0,0 +1,13 @@
+package com.ecureuill.rpgbattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgbattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgbattleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecureuill/rpgbattle/RpgbattleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecureuill.rpgbattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgbattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
