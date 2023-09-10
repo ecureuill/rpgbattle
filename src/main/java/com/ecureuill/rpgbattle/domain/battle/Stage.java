838a0520e260dafef5c8c3c0fe9c3d3f70736bd1
Camilla <logikasciuro@gmail.com>
2023-09-10 18:44:36 +0000

    🏷️ (Battle) Add description to Stage enum

--- src/main/java/com/ecureuill/rpgbattle/domain/battle/Stage.java
+++ src/main/java/com/ecureuill/rpgbattle/domain/battle/Stage.java
@@ -1,5 +1,16 @@
 package com.ecureuill.rpgbattle.domain.battle;
 
 public enum Stage {
-  CHARACTER_SELECTION, INITIATIVE, TURNS, END
+  CHARACTER_SELECTION("Character selection phase"),
+  INITIATIVE("Initiative phase"), 
+  TURNS("Turn phase"), 
+  END("Battle finished");
+
+  private final String name;
+  Stage(String name) {
+    this.name = name;
+  }
+  public String getName() {
+    return name;
+  }
 }
