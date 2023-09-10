26b593c83d1f6f6c47c2ae64cf220c579719e313
Camilla <logikasciuro@gmail.com>
2023-09-10 20:24:49 +0000

    🏷️ (User) Create user domain

---
+++ src/main/java/com/ecureuill/rpgbattle/domain/user/User.java
@@ -0,0 +1,52 @@
+package com.ecureuill.rpgbattle.domain.user;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.UUID;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+  @Column(unique = true)
+  private String username;
+  @Column(unique = true)
+  private String email;
+  private String password;
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return Collections.emptyList();
+  }
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }  
+}
